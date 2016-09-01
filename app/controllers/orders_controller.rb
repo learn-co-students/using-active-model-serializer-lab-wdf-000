c0e8df59192bc718c142b67de6f2808740d9b89d
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-09-01 15:21:54 -0400

    Done

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,4 +4,13 @@ class OrdersController < ApplicationController
     @orders = Order.all
   end
 
+  def show
+    @order = Order.find(params[:id])
+    byebug
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @order}
+    end
+  end
+
 end
